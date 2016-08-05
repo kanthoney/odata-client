24bdee55a926cfec1490a8bb96f71fd3cd48fdc8
Kevin Anthoney <kevin@kanthoney.co.uk>
2016-08-06 00:01:56 +0100

    add another count spec

--- spec/count_spec.js
+++ spec/count_spec.js
@@ -18,5 +18,10 @@ describe('count tests', function() {
     expect(odata.count().query()).toEqual('https://example.com/Customers?%24count');
   });
 
+  it('should get a count of Customers with balance > 1000', function() {
+    expect(odata.filter('balance', '>', 1000).count().query())
+      .toEqual('https://example.com/Customers?%24count&%24filter=balance%20gt%201000');
+  });
+
 });
 
